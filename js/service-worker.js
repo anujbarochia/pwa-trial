d699d3b192b5f380cc1ad583544b26e703d85cff
anujbarochia <anujbarochia24@gmail.com>
2025-01-02 17:42:52 +0530

    removed caching

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,12 +1,5 @@
-const CACHE_NAME = 'my-pwa-cache-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/css/styles.css',
-    '/js/main.js',
-    '/images/icon-192x192.png',
-    '/images/icon-512x512.png'
-];
+const CACHE_NAME = 'my-pwa-cache';
+const urlsToCache = [];
 
 self.addEventListener('install', event => {
     event.waitUntil(
