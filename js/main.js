5c53a7b2efd51414b2f666184e66f1297a49ca0a
anujbarochia <anujbarochia24@gmail.com>
2025-01-02 17:34:13 +0530

    removed buttons

--- js/main.js
+++ js/main.js
@@ -7,34 +7,3 @@ if ('serviceWorker' in navigator) {
         });
     });
   }
-  
-  let deferredPrompt;
-  window.addEventListener('beforeinstallprompt', (e) => {
-    e.preventDefault();
-    deferredPrompt = e;
-    showInstallPrompt();
-  });
-  
-  function showInstallPrompt() {
-    if (deferredPrompt) {
-      deferredPrompt.prompt();
-      deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
-    }
-  }
-  
-  // Increment number functionality
-  let number = 0;
-  const incrementButton = document.getElementById('increment-button');
-  const numberDisplay = document.getElementById('number-display');
-  
-  incrementButton.addEventListener('click', () => {
-    number += 1;
-    numberDisplay.textContent = number;
-  });
