4223953882c9bd59a3ea651158af50a15e3d8eef
anujbarochia <anujbarochia24@gmail.com>
2025-01-02 16:48:46 +0530

    Add to homescreen prompt add

--- js/main.js
+++ js/main.js
@@ -7,3 +7,37 @@ if ('serviceWorker' in navigator) {
       });
   });
 }
+
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+});
+
+function showInstallPromotion() {
+  const installButton = document.createElement('button');
+  installButton.innerText = 'Install PWA';
+  installButton.style.position = 'fixed';
+  installButton.style.bottom = '10px';
+  installButton.style.right = '10px';
+  document.body.appendChild(installButton);
+
+  installButton.addEventListener('click', () => {
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+      installButton.remove();
+    });
+  });
+}
